d51d95f794f89b4f4ce4c4d0ed7c8d26681a96d9
evegyzbirun <zbirunzbirun@gmail.com>
2022-10-22 18:34:52 -0700

    added erbs views for product.

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -5,17 +5,42 @@ class ProductsController < ApplicationController
   end
 
   def new
-    @products = Product.new
+    @product = Product.new
     render :new
   end
 
   def create
-    @products = Product.new(product_params)
+    @product = Product.new(product_params)
     if @product.save
       redirect_to products_path
     else
       render :new
     end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+    render :edit
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    render :show
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
   end
 
   private
