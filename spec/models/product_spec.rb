63b1bd505c9a462f823cdee894f94a55c41c0974
evegyzbirun <zbirunzbirun@gmail.com>
2022-10-22 22:50:39 -0700

    added few test for product spec, create method works for product and review

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,9 +5,15 @@ describe Product do
   it { should validate_length_of(:name).is_at_most(100) }
 
   it("has many reviews") do
-    product = Product.create(name: "product")
-    review1 = Review.create(author: "review1", product_id: product.id)
-    review2 = Review.create(author: "review2", product_id: product.id)
+    product = Product.create({name: "giant steps", cost: 5, country_of_origin: "USA"})
+    review1 = Review.create(author: "review12", content_body: "We love it", rating: 3, product_id: product.id)
+
+    review2 = Review.create(author: "review22", content_body: "We hate it", rating: 1, product_id: product.id)
     expect(product.reviews()).to(eq([review1, review2]))
+  end
+
+  it("enter info of an product") do
+    product = Product.create({name: "giant steps", cost: 5, country_of_origin: "USA"})
+    expect(product.name()).to(eq("giant steps"))
   end
 end
