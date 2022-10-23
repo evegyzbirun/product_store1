7843369e6c423297330d149e4b86fe64f7ba25ab
evegyzbirun <zbirunzbirun@gmail.com>
2022-10-22 17:35:38 -0700

    test for belong_to product and review passed

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
 
 describe Product do
+  it { should have_many(:reviews) }
+
+
   it("has many reviews") do
     product = Product.create(name: "product")
-    review1 = Review.create(name: "review1", product_id: product.id)
-    review2 = Review.create(name: "review2", product_id: product.id)
+    review1 = Review.create(author: "review1", product_id: product.id)
+    review2 = Review.create(author: "review2", product_id: product.id)
     expect(product.reviews()).to(eq([review1, review2]))
   end
 end
