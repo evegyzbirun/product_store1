d38a8735be45c9f175b2ac36162812af35e35721
evegyzbirun <zbirunzbirun@gmail.com>
2022-10-22 19:57:45 -0700

    website let user to add a product.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'products#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: 'products#index'
+  
   resources :products do
     resources :reviews
   end
