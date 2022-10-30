137134f4a8921db70c5c058ed6f64a4c230e2a79
evegyzbirun <zbirunzbirun@gmail.com>
2022-10-30 08:04:51 -0700

    made scope for recent 3 product..

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,17 +1,23 @@
 class Product < ApplicationRecord
   
-  scope :three_most_recent, -> { order(created_at: :desc).limit(3)}
-
   has_many :reviews, dependent: :destroy
   validates :name, presence: true
   validates :cost, presence: true
   validates :country_of_origin, presence: true
   validates_length_of :name, maximum: 100
-
   before_save(:titleize_product)
 
-  
 
+  scope :three_most_recent, -> { order(created_at: :desc).limit(3)}
+  scope :most_tasks, -> {(
+    select("products.id, products.name, products.role, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("products.id")
+    .order("reviews_count DESC")
+    .limit(3)
+    )}
+
+    scope :name, -> (name_parameter) { where(name: name_parameter) } 
   private
     def titleize_product
       self.name = self.name.titleize
